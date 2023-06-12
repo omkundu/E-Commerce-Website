0a4ea3505c686ee263e3506a46e9ee74c7d81d3f
omkundu <omkundu156@gmail.com>
2023-06-12 10:59:46 +0530

    Checkout-Page-done

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -32,8 +32,28 @@ const products = [
   // More products...
 ];
 
+const addresses=[
+  {
+    name:"jhon wick",
+    street:"11th Main",
+    city:"Delhi",
+    pincode:11001,
+    state:"Delhi",
+    phone:1234567789
+  },
+  {
+    name:"jhon Deo",
+    street:"15th Main",
+    city:"Bengalore",
+    pincode:98768,
+    state:"Kerala",
+    phone:7387835436
+  },
+]
+
+
 function Checkout() {
-  
+
   // const count = useSelector(selectCount);
   const dispatch = useDispatch();
   return (
@@ -43,10 +63,10 @@ function Checkout() {
       <div className="mx-auto mx-w-7xl px-4 sm:px-7 lg:px-8">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-5">
           <div className="lg:col-span-3">
-            <form className="bg-white px-3">
+            <form className="bg-white px-7 mt-12 py-12" >
               <div className="space-y-12">
                 <div className="border-b border-gray-900/10 pb-12">
-                  <h2 className="text-base font-semibold leading-7 text-gray-900">
+                  <h2 className="font-semibold leading-7 text-2xl text-gray-900">
                     Personal Information
                   </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-600">
@@ -202,14 +222,58 @@ function Checkout() {
                     </div>
                   </div>
                 </div>
+                 
+                <div className="mt-6 flex items-center justify-end gap-x-6">
+                <button
+                  type="button"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Reset
+                </button>
+                <button
+                  type="submit"
+                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                >
+                  Save Address
+                </button>
+              </div>
+
 
                 <div className="border-b border-gray-900/10 pb-12">
                   <h2 className="text-base font-semibold leading-7 text-gray-900">
-                    Address
+                   Addresses
                   </h2>
                   <p className="mt-1 text-sm leading-6 text-gray-600">
                     Choose from Existing addresses
                   </p>
+
+                  <ul role="list">
+      {addresses.map((address) => (
+        <li key={address.email} className="flex justify-between gap-x-6 px-7 py-5  border-solid border-2 border-gray-200">
+          <div className="flex gap-x-4">
+           
+          <input
+                            name="address"
+                            type="radio"
+                            className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+            />
+
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-900">{address.name}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{address.street}</p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">{address.pincode}</p>
+
+            </div>
+          </div>
+          <div className="hidden sm:flex sm:flex-col sm:items-end">
+            <p className="text-sm leading-6 text-gray-900">Phone {address.phone}</p>
+            <p className="text-sm leading-6 text-gray-500">{address.city}</p>
+
+              
+          </div>
+        </li>
+      ))}
+    </ul>
 
                   <div className="mt-10 space-y-10">
                    
@@ -256,26 +320,13 @@ function Checkout() {
                 </div>
               </div>
 
-              <div className="mt-6 flex items-center justify-end gap-x-6">
-                <button
-                  type="button"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-                >
-                  Cancel
-                </button>
-                <button
-                  type="submit"
-                  className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Save
-                </button>
-              </div>
+             
             </form>
           </div>
           <div className="lg:col-span-2">
-          <div className="mx-auto mt-9 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="mx-auto mt-9 bg-white max-w-7xl px-0 sm:px-0 lg:px-0">
      
-     <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+     <div className="border-t border-gray-200 px-4 py-5 sm:px-3 mt-12" >
 
      <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
     <h1 className="text-4xl my-12 font-bold tracking-tight text-gray-900">
@@ -343,11 +394,11 @@ function Checkout() {
           Shipping and taxes calculated at checkout.
         </p>
         <div className="mt-6">
-          <Link to='/checkout'
+          <Link to='/pay'
             
             className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
           >
-            Checkout
+            Pay and Order
           </Link>
         </div>
         <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
