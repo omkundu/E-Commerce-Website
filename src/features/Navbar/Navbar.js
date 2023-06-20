49235ca07ada0290545dec91e1516155a8fa5c65
omkundu <omkundu156@gmail.com>
2023-06-20 12:47:46 +0530

    total-amount-calculated-total-quantity-updated

--- src/features/Navbar/Navbar.js
+++ src/features/Navbar/Navbar.js
@@ -80,10 +80,11 @@ function Navbar ({children}) {
                         
                       </button>
                       </Link>
-                      <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                      {items.length}
+                      {items.length>0&& <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                     {items.length}
 
-                    </span>
+                    </span>}
+                    
 
                       {/* Profile dropdown */}
                       <Menu as="div" className="relative ml-3">
@@ -171,9 +172,9 @@ function Navbar ({children}) {
                       
                     </button>
                     </Link>
-                    <span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                    {items.length>0&&<span className="inline-flex items-center rounded-md mb-7 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
                         {items.length}
-                    </span>
+                    </span>}
                   </div>
                   <div className="mt-3 space-y-1 px-2">
                     {userNavigation.map((item) => (
--- src/features/cart/Cart.js
+++ src/features/cart/Cart.js
@@ -1,43 +1,47 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, incrementAsync, selectCount } from "./cartSlice";
+import { increment, incrementAsync, selectCount, selectItems } from "./cartSlice";
 
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import { Link } from "react-router-dom";
 
-const products = [
-  {
-    id: 1,
-    name: "Throwback Hip Bag",
-    href: "#",
-    color: "Salmon",
-    price: "$90.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
-    imageAlt:
-      "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
-  },
-  {
-    id: 2,
-    name: "Medium Stuff Satchel",
-    href: "#",
-    color: "Blue",
-    price: "$32.00",
-    quantity: 1,
-    imageSrc:
-      "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
-    imageAlt:
-      "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
-  },
-  // More products...
-];
+// const products = [
+//   {
+//     id: 1,
+//     name: "Throwback Hip Bag",
+//     href: "#",
+//     color: "Salmon",
+//     price: "$90.00",
+//     quantity: 1,
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-01.jpg",
+//     imageAlt:
+//       "Salmon orange fabric pouch with match zipper, gray zipper pull, and adjustable hip belt.",
+//   },
+//   {
+//     id: 2,
+//     name: "Medium Stuff Satchel",
+//     href: "#",
+//     color: "Blue",
+//     price: "$32.00",
+//     quantity: 1,
+//     imageSrc:
+//       "https://tailwindui.com/img/ecommerce-images/shopping-cart-page-04-product-02.jpg",
+//     imageAlt:
+//       "Front of satchel with blue canvas body, black straps and handle, drawstring top, and front zipper pouch.",
+//   },
+//   // More products...
+// ];
 
 export default function Cart() {
   // const count = useSelector(selectCount);
   const dispatch = useDispatch();
+  const items=useSelector(selectItems)
+  const totalAmount=items.reduce((amount,item)=>item.price*item.quantity+amount,0)
+  const totalItems=items.reduce((total,item)=>item.quantity+total,0)
+
 
   return (
         <div className="mx-auto mt-9 bg-white max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -52,12 +56,12 @@ export default function Cart() {
        <div className="mt-8">
          <div className="flow-root">
            <ul role="list" className="-my-6 divide-y divide-gray-200">
-             {products.map((product) => (
+             {items.map((product) => (
                <li key={product.id} className="flex py-6">
                  <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
                    <img
-                     src={product.imageSrc}
-                     alt={product.imageAlt}
+                     src={product.thumbnail}
+                     alt={product.title}
                      className="h-full w-full object-cover object-center"
                    />
                  </div>
@@ -66,12 +70,12 @@ export default function Cart() {
                    <div>
                      <div className="flex justify-between text-base font-medium text-gray-900">
                        <h3>
-                         <a href={product.href}>{product.name}</a>
+                         <a href={product.href}>{product.title}</a>
                        </h3>
-                       <p className="ml-4">{product.price}</p>
+                       <p className="ml-4">${product.price}</p>
                      </div>
                      <p className="mt-1 text-sm text-gray-500">
-                       {product.color}
+                       {product.brand}
                      </p>
                    </div>
                    <div className="flex flex-1 items-end justify-between text-sm">
@@ -105,7 +109,11 @@ export default function Cart() {
        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
          <div className="flex justify-between text-base font-medium text-gray-900">
            <p>Subtotal</p>
-           <p>$262.00</p>
+           <p>${totalAmount}</p>
+         </div>
+         <div className="flex justify-between text-base font-medium text-gray-900">
+           <p>Total Items in Cart</p>
+           <p>{totalItems}</p>
          </div>
          <p className="mt-0.5 text-sm text-gray-500">
            Shipping and taxes calculated at checkout.
