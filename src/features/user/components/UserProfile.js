c2f4afacf3a4349522afdf0ba449ae822acc7f33
omkundu <omkundu156@gmail.com>
2023-06-28 11:36:34 +0530

    new-address-edit-eddress-done

--- src/features/user/components/UserProfile.js
+++ src/features/user/components/UserProfile.js
@@ -10,6 +10,8 @@ function UserProfile() {
   const [paymentMethod, setPaymentMethod] = useState("cash");
 
   const [selectedEditIndex,setSelectedEditIndex]=useState(-1)
+  const [showAddAddressForm,setshowAddAddressForm]=useState(false)
+
 
   const {
     handleSubmit,
@@ -49,8 +51,12 @@ const handleEditForm=(index)=>{
   setValue("state",address.state)
   setValue("street",address.street)
 
+}
 
-
+const handleAdd=(address)=>{
+  const newUser={...user,addresses:[...user.addresses,address]}
+  dispatch(updatedUserAsync(newUser))
+  setshowAddAddressForm(false)
 }
 
   return (
@@ -69,10 +75,286 @@ const handleEditForm=(index)=>{
               <div className="mt-8"></div>
 
               <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+              <button
+              onClick={e=>{setshowAddAddressForm(true);setSelectedEditIndex(-1)}}
+                        type="submit"
+                        className="rounded-md my-5 bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Add New Address
+                      </button>
+
+                      {showAddAddressForm?
+                  <form
+                  className="bg-white px-7 mt-12 py-12"
+                  noValidate
+                  onSubmit={handleSubmit((data) => {
+                    console.log(data);
+                   handleAdd(data)
+                    reset();
+                  })}
+                >
+                  <div className="space-y-12">
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <h2 className="font-semibold leading-7 text-2xl text-gray-900">
+                        Personal Information
+                      </h2>
+                      <p className="mt-1 text-sm leading-6 text-gray-600">
+                        Use a permanent address where you can receive mail.
+                      </p>
+
+                      <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="name"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Full name
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("name", {
+                                required: "name is required",
+                              })}
+                              id="name"
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-4">
+                          <label
+                            htmlFor="email"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Email address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              id="email"
+                              {...register("email", {
+                                required: "email is required",
+                              })}
+                              type="email"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-3">
+                          <label
+                            htmlFor="phone"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Phone
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="tel"
+                              {...register("Phone", {
+                                required: "Phone is required",
+                              })}
+                              id="phone"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="col-span-full">
+                          <label
+                            htmlFor="street-address"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            Street address
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("street", {
+                                required: "street-address is required",
+                              })}
+                              id="street"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2 sm:col-start-1">
+                          <label
+                            htmlFor="city"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            City
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("city", {
+                                required: "city is required",
+                              })}
+                              id="city"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                          <label
+                            htmlFor="state"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            State / Province
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("state", {
+                                required: "state is required",
+                              })}
+                              id="state"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+
+                        <div className="sm:col-span-2">
+                          <label
+                            htmlFor="pinCode"
+                            className="block text-sm font-medium leading-6 text-gray-900"
+                          >
+                            ZIP / Postal code
+                          </label>
+                          <div className="mt-2">
+                            <input
+                              type="text"
+                              {...register("pinCode", {
+                                required: "pinCode is required",
+                              })}
+                              id="pinCode"
+
+                              className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="mt-6 flex items-center justify-end gap-x-6">
+                  
+                      <button
+                        type="submit"
+                        className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        Add Address
+                      </button>
+                    </div>
+
+                    <div className="border-b border-gray-900/10 pb-12">
+                      <h2 className="text-base font-semibold leading-7 text-gray-900">
+                        Addresses
+                      </h2>
+                      <p className="mt-1 text-sm leading-6 text-gray-600">
+                        Choose from Existing addresses
+                      </p>
+
+                      <ul role="list">
+                        {user.addresses.map((address, index) => (
+                          <li
+                            key={index}
+                            className="flex justify-between gap-x-6 px-7 py-5  border-solid border-2 border-gray-200"
+                          >
+                            <div className="flex gap-x-4">
+                              {/* <input
+                                onChange={handleAddress}
+                                name="address"
+                                type="radio"
+                                value={index}
+                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                              /> */}
+
+                              <div className="min-w-0 flex-auto">
+                                <p className="text-sm font-semibold leading-6 text-gray-900">
+                                  {address.name}
+                                </p>
+                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                                  {address.street}
+                                </p>
+                                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                                  {address.pinCode}
+                                </p>
+                              </div>
+                            </div>
+                            <div className="hidden sm:flex sm:flex-col sm:items-end">
+                              <p className="text-sm leading-6 text-gray-900">
+                                Phone {address.phone}
+                              </p>
+                              <p className="text-sm leading-6 text-gray-500">
+                                {address.city}
+                              </p>
+                            </div>
+                          </li>
+                        ))}
+                      </ul>
+
+                      <div className="mt-10 space-y-10">
+                        <fieldset>
+                          <legend className="text-sm font-semibold leading-6 text-gray-900">
+                            Payment Methods
+                          </legend>
+                          <p className="mt-1 text-sm leading-6 text-gray-600">
+                            Choose One
+                          </p>
+                          <div className="mt-6 space-y-6">
+                            <div className="flex items-center gap-x-3">
+                              <input
+                                id="case"
+                                name="payments"
+                                type="radio"
+                                onChange={handlePayment}
+                                value="cash"
+                                checked={paymentMethod === "cash"}
+                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                              />
+                              <label
+                                htmlFor="cash"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Cash
+                              </label>
+                            </div>
+                            <div className="flex items-center gap-x-3">
+                              <input
+                                id="card"
+                                name="payments"
+                                type="radio"
+                                value="card"
+                                checked={paymentMethod === "card"}
+                                onChange={handlePayment}
+                                className="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                              />
+                              <label
+                                htmlFor="card"
+                                className="block text-sm font-medium leading-6 text-gray-900"
+                              >
+                                Card Payment
+                              </label>
+                            </div>
+                          </div>
+                        </fieldset>
+                      </div>
+                    </div>
+                  </div>
+                </form>:null}
                 <p className="mt-0.5 text-sm text-gray-500">Your Addresses :</p>
                 {user.addresses.map((address,index) => (
                   <div>
-                  {selectedEditIndex===index?<form
+                  {selectedEditIndex===index?
+                  <form
                   className="bg-white px-7 mt-12 py-12"
                   noValidate
                   onSubmit={handleSubmit((data) => {
