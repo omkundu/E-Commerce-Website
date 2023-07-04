33b577ef802c90e6569090e31fd03a87c9c1ddbb
omkundu <omkundu156@gmail.com>
2023-07-04 12:58:22 +0530

    admin-page-done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../features/Navbar/Navbar'
 import ProductList from '../features/Product-List/components/ProductList'
+import { Link } from 'react-router-dom'
 const Home = () => {
   return (
     <div>
       <Navbar>
         <ProductList></ProductList>
       </Navbar>
+      <Link to="/admin">Admin</Link>
     </div>
   )
 }
