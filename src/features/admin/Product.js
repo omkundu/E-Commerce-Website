941e1e1552b554ed15dea3ebebab9e30f1043488
omkundu <omkundu156@gmail.com>
2023-07-06 11:04:59 +0530

    admin-page-done

--- src/features/admin/Product.js
+++ src/features/admin/Product.js
@@ -13,7 +13,7 @@ function ProductForm() {
          
 
             <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-              <div className="sm:col-span-4">
+              <div className="sm:col-span-6">
                 <label
                   htmlFor="title"
                   className="block text-sm font-medium leading-6 text-gray-900"
@@ -34,6 +34,7 @@ function ProductForm() {
               </div>
 
               <div className="col-span-full">
+
                 <label
                   htmlFor="description"
                   className="block text-sm font-medium leading-6 text-gray-900"
@@ -111,6 +112,90 @@ function ProductForm() {
                   </div>
                 </div>
               </div>
+            
+
+              <div className="sm:col-span-6">
+                <label
+                  htmlFor="thumbnail"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Thumbnail
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                   
+                    <input
+                      type="text"
+                      name="thumbnail"
+                      id="thumbnail"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-6">
+                <label
+                  htmlFor="image1"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Image 1
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                   
+                    <input
+                      type="text"
+                      name="image1"
+                      id="image1"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="sm:col-span-6">
+                <label
+                  htmlFor="image2"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Image 2
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                   
+                    <input
+                      type="text"
+                      name="image2"
+                      id="image2"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
+
+              <div className="sm:col-span-6">
+                <label
+                  htmlFor="image3"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Image 3
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600">
+                   
+                    <input
+                      type="text"
+                      name="image3"
+                      id="image3"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
 
          
             </div>
