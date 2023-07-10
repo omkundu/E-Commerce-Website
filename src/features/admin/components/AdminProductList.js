88f9f4aa5a8c4db08ddd37c6b2b93af1e7e83eb3
omkundu <omkundu156@gmail.com>
2023-07-10 10:44:19 +0530

    admin-page-done

--- src/features/admin/components/AdminProductList.js
+++ src/features/admin/components/AdminProductList.js
@@ -571,9 +571,9 @@ function ProductGrid({ products }) {
                   </div>
                 </Link>
                 <div>
-                          <Link to={`admin/product-form/edit/${products.id}`}
+                          <Link to={`/admin/product-form/edit/${product.id}`}
 
-                           className="rounded-md my-5 bg-indigo-600 px-3 pt-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                           className="rounded-md my-5 bg-indigo-600 px-1 pt-1 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                           >Edit Product
                           </Link>
                         </div>
