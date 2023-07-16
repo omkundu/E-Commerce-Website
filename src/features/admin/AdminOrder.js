36e66480086fcf8fe77bbd4f34fca9c8de5a0e4a
omkundu <omkundu156@gmail.com>
2023-07-16 17:09:20 +0530

    admin-order-done

--- src/features/admin/AdminOrder.js
+++ src/features/admin/AdminOrder.js
@@ -3,7 +3,7 @@ import { fetchProductsByFiltersAsync } from "./ProductListSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { ITEM_PER_PAGE } from "../../app/constants";
 import { fetchAllOrdersAsync, selectOrders, selectTotalOrders } from "../order/orderSlice";
-
+import {EyeIcon, PencilIcon} from "@heroicons/react/24/outline"
 function AdminOrder() {
     const [page,setPage]=useState(1)
     const dispatch=useDispatch()
@@ -12,10 +12,18 @@ function AdminOrder() {
 
     useEffect(() => {
         const pagination = { _page: page, _limit: ITEM_PER_PAGE };
-        // dispatch(fetchAllProductsAsync())
         dispatch(fetchAllOrdersAsync( pagination ));
         
       }, [dispatch, page]);
+
+      const handleEdit=()=>{
+      console.log("HandleEdit")
+      }
+
+
+      const handleShow=()=>{
+        console.log("handleShow")
+      }
     
     return (
 
@@ -23,14 +31,15 @@ function AdminOrder() {
         {/* component */}
         <div className="overflow-x-auto">
           <div className=" bg-gray-100 flex items-center justify-center bg-gray-100 font-sans overflow-hidden">
-            <div className="w-full">
+            <div className="w-full lg:w-5/6">
               <div className="bg-white shadow-md rounded my-6">
                 <table className="min-w-max w-full table-auto">
                   <thead>
                     <tr className="bg-gray-200 text-gray-600 uppercase text-sm leading-normal">
-                      <th className="py-3 px-6 text-left">Order Number</th>
+                      <th className="py-3 px-6 text-left">Order#</th>
                       <th className="py-3 px-6 text-left">Items</th>
                       <th className="py-3 px-6 text-center">Total Amount</th>
+                      <th className="py-3 px-6 text-center">Shipping Address</th>
                       <th className="py-3 px-6 text-center">Status</th>
                       <th className="py-3 px-6 text-center">Actions</th>
                     </tr>
@@ -46,91 +55,55 @@ function AdminOrder() {
                         </div>
                       </td>
                       <td className="py-3 px-6 text-left">
-                        {orders?.items?.map(item=><div className="flex items-center">
+                        {order?.items?.map(item=><div className="flex items-center">
                           <div className="mr-2">
                             <img
                               className="w-6 h-6 rounded-full"
                               src={item.thumbnail}
                             />
                           </div>
-                          <span>{item.title}</span>
+                          <span>{item.title} - {item.quantity} - ${item.price}</span>
                         </div>)}
                        
                       </td>
                       <td className="py-3 px-6 text-center">
                         <div className="flex items-center justify-center">
-                          <img
-                            className="w-6 h-6 rounded-full border-gray-200 border transform hover:scale-125"
-                            src="https://randomuser.me/api/portraits/men/1.jpg"
-                          />
-                          <img
-                            className="w-6 h-6 rounded-full border-gray-200 border -m-1 transform hover:scale-125"
-                            src="https://randomuser.me/api/portraits/women/2.jpg"
-                          />
-                          <img
-                            className="w-6 h-6 rounded-full border-gray-200 border -m-1 transform hover:scale-125"
-                            src="https://randomuser.me/api/portraits/men/3.jpg"
-                          />
+                      ${order.totalAmount}
+                        </div>
+                      </td>
+                      <td className="py-3 px-6 text-center">
+                        <div className="flex items-center justify-center">
+                  
+                      {order.selectedAddress ? (
+                          <div className="">
+                            <div>
+                            <strong>{order.selectedAddress.name}</strong>,
+                            </div>
+                             <div>{order.selectedAddress.street}</div>
+                             <div>{order.selectedAddress.city}</div>
+                             <div>{order.selectedAddress.state}</div>
+                             <div>{order.selectedAddress.pinCode}, </div>
+                             <div>{order.selectedAddress.phone}</div>
+
+                          </div>
+                        ) : null}
                         </div>
                       </td>
                       <td className="py-3 px-6 text-center">
                         <span className="bg-purple-200 text-purple-600 py-1 px-3 rounded-full text-xs">
-                          Active
+                          {order.status}
                         </span>
                       </td>
                       <td className="py-3 px-6 text-center">
                         <div className="flex item-center justify-center">
-                          <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                              />
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                              />
-                            </svg>
+                          <div className="w-6 mr-4 transform hover:text-purple-500 hover:scale-120">
+                           <EyeIcon className="w-8 h-8" onClick={e=>handleShow(order)}></EyeIcon>
                           </div>
-                          <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                              />
-                            </svg>
+                          <div className="w-6 mr-2 transform hover:text-purple-500 hover:scale-120">
+                          <PencilIcon className="w-8 h-8" onClick={e=>handleEdit(order)}></PencilIcon>
+
                           </div>
-                          <div className="w-4 mr-2 transform hover:text-purple-500 hover:scale-110">
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              stroke="currentColor"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth={2}
-                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                              />
-                            </svg>
-                          </div>
+                        
                         </div>
                       </td>
                     </tr>)}
